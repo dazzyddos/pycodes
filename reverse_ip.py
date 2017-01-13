8f6719f9cafc059e9bda71bea6479ad6af44f20c
hackerarunnair <hackerarunnair@gmail.com>
2017-01-13 20:39:16 +0530

    Add files via upload

---
+++ reverse_ip.py
@@ -0,0 +1,18 @@
+import requests
+import socket
+import sys
+
+site = "http://api.hackertarget.com/reverseiplookup"
+
+if len(sys.argv) != 2:
+	print "Usage: reverse_ip.py <site>\n"
+	sys.exit(0)
+
+url = sys.argv[1]
+
+ip = socket.gethostbyname(url)
+data = {'q':ip}
+
+resp = requests.get(site, params=data)
+
+print resp.text
